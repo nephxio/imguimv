bdcde171bead4389a8ccca1f75ca866f6dc4cdb9
Phil Wallace <phil.wallace83@gmail.com>
2025-03-02 17:30:54 -0500

    Update .gitignore and premake script for more error handling

--- premake5.lua
+++ premake5.lua
@@ -1,48 +1,78 @@
+-- ImGui version 1.89.9 (or specify the actual version you're using)
 project "ImGui"
-	kind "StaticLib"
-	language "C++"
+kind "StaticLib"
+language "C++"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+-- Directory variables for easier maintenance
+local IMGUI_DIR = "."
+local BACKENDS_DIR = IMGUI_DIR .. "/backends"
 
-	files
-	{
-        "imconfig.h",
-        "imgui.cpp",
-        "imgui.h",
-        "imgui_demo.cpp",
-        "imgui_draw.cpp",
-        "imgui_internal.h",
-        "imgui_tables.cpp",
-        "imgui_widgets.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h",
-        "./backends/imgui_impl_glfw.h",
-        "./backends/imgui_impl_vulkan.h",
-        "./backends/imgui_impl_glfw.cpp",
-        "./backends/imgui_impl_vulkan.cpp"
-	}
-	
-	includedirs
-	{
-		".",
-		"%{wks.location}/external/glfw/include",
-		os.getenv("VULKAN_SDK") .. "/Include"
-	}
-	
+targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++20"
-		staticruntime "On"
+-- Vulkan SDK path handling with error checking
+local vulkanSDK = os.getenv("VULKAN_SDK")
+if not vulkanSDK then
+    error("VULKAN_SDK environment variable is not set")
+end
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-		linkoptions { "/NODEFAULTLIB:LIBCMTD" }
+files
+{
+    IMGUI_DIR .. "/imconfig.h",
+    IMGUI_DIR .. "/imgui.cpp",
+    IMGUI_DIR .. "/imgui.h",
+    IMGUI_DIR .. "/imgui_demo.cpp",
+    IMGUI_DIR .. "/imgui_draw.cpp",
+    IMGUI_DIR .. "/imgui_internal.h",
+    IMGUI_DIR .. "/imgui_tables.cpp",
+    IMGUI_DIR .. "/imgui_widgets.cpp",
+    IMGUI_DIR .. "/imstb_rectpack.h",
+    IMGUI_DIR .. "/imstb_textedit.h",
+    IMGUI_DIR .. "/imstb_truetype.h",
+    BACKENDS_DIR .. "/imgui_impl_glfw.h",
+    BACKENDS_DIR .. "/imgui_impl_vulkan.h",
+    BACKENDS_DIR .. "/imgui_impl_glfw.cpp",
+    BACKENDS_DIR .. "/imgui_impl_vulkan.cpp"
+}
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-		linkoptions { "/NODEFAULTLIB:LIBCMTD" }
+includedirs
+{
+    IMGUI_DIR,
+    "%{wks.location}/external/glfw/include",
+    vulkanSDK .. "/Include"
+}
+
+-- Common settings across all platforms
+warnings "Extra"
+
+-- Platform specific configurations
+filter "system:windows"
+    systemversion "latest"
+    cppdialect "C++20"
+    defines { "IMGUI_API=__declspec(dllexport)" }
+    
+filter "system:linux"
+    systemversion "latest"
+    cppdialect "C++20"
+    defines { "IMGUI_API=" }
+    
+filter "system:macosx"
+    systemversion "latest"
+    cppdialect "C++20"
+    defines { "IMGUI_API=" }
+
+-- Configuration specific settings
+filter "configurations:Debug"
+    staticruntime "On"
+    runtime "Debug"
+    symbols "on"
+    defines { "IMGUI_DEBUG" }
+    
+filter "configurations:Release"
+    staticruntime "On"
+    runtime "Release"
+    optimize "on"
+    
+-- Windows specific linker options
+filter { "system:windows", "configurations:Debug or Release" }
+    linkoptions { "/NODEFAULTLIB:LIBCMTD" }
